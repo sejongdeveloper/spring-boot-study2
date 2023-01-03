50069abd4757a9c79693fc85fba8f0cb47005ff0
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-03 22:22:16 +0900

    SpringApplication 2부 : CommandLineRunner

--- src/main/java/me/whiteship/springinit/SampleListener.java
+++ src/main/java/me/whiteship/springinit/SampleListener.java
@@ -1,15 +1,15 @@
 package me.whiteship.springinit;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
-public class SampleListener implements ApplicationRunner {
+public class SampleListener implements CommandLineRunner {
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("foo: " + args.containsOption("foo"));
-        System.out.println("bar: " + args.containsOption("bar"));
+    public void run(String... args) throws Exception {
+        Arrays.stream(args).forEach(System.out::println);
     }
 }
