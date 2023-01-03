a552a4a2125056e929f0ae0338db284ced699c1b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-03 22:23:26 +0900

    SpringApplication 2부 : ApplicationRunner

--- src/main/java/me/whiteship/springinit/SampleListener.java
+++ src/main/java/me/whiteship/springinit/SampleListener.java
@@ -1,15 +1,19 @@
 package me.whiteship.springinit;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
-public class SampleListener implements CommandLineRunner {
+@Order(1)
+public class SampleListener implements ApplicationRunner {
 
     @Override
-    public void run(String... args) throws Exception {
-        Arrays.stream(args).forEach(System.out::println);
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("@Order 순서가 낮을수록 먼저 실행됨");
     }
 }
