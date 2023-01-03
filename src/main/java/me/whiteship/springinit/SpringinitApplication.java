a27b9f84a0a3518af59ba4434cea46999083c7d3
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-03 21:24:39 +0900

    Initial commit

---
+++ src/main/java/me/whiteship/springinit/SpringinitApplication.java
@@ -0,0 +1,27 @@
+package me.whiteship.springinit;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
+
+@SpringBootApplication
+public class SpringinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SpringinitApplication.class);
+//		app.setBannerMode(Banner.Mode.OFF);
+		app.setBanner(new Banner() {
+			@Override
+			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+				out.println("==========================");
+				out.println("WHITESHIP");
+				out.println("==========================");
+			}
+		});
+		app.run(args);
+	}
+
+}
