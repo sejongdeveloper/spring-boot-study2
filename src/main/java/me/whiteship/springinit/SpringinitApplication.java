a4fd8af631f7a5df4ca4c65c91956d0c4e241b3b
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-03 23:14:43 +0900

    외부 설정 2부

--- src/main/java/me/whiteship/springinit/SpringinitApplication.java
+++ src/main/java/me/whiteship/springinit/SpringinitApplication.java
@@ -3,8 +3,10 @@ package me.whiteship.springinit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KeesunProperties.class)
 public class SpringinitApplication {
 
 	public static void main(String[] args) {
