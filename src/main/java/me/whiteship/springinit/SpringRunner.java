179a354ef77148b3a4c78e774508e56d526cef8d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-04 22:48:29 +0900

    로깅 1부: 스프링 부트 기본 로거 설정

--- src/main/java/me/whiteship/springinit/SpringRunner.java
+++ src/main/java/me/whiteship/springinit/SpringRunner.java
@@ -1,5 +1,7 @@
 package me.whiteship.springinit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,18 +9,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringRunner implements ApplicationRunner {
-
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private String hello;
 
     @Autowired
     private KeesunProperties keesunProperties;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("=======================");
-        System.out.println(hello);
-        System.out.println(keesunProperties.getName());
-        System.out.println(keesunProperties.getFullName());
-        System.out.println("=======================");
+        logger.info("=====================");
+        logger.info(hello);
+        logger.info(keesunProperties.getName());
+        logger.info(keesunProperties.getFullName());
+        logger.info("=====================");
     }
 }
